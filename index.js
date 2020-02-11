0870577dcccf8277aa4e8922caa14ccaa6abf8c8
Aria Sadeghi <ariasadeghi@gmail.com>
2020-02-11 15:09:18 -0800

    installed inquirer packages

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const axios = require('axios');
 
+
+
 axios.get('https://api.github.com/users/ariasadeghi530')
   .then(function (response) {
     // handle success
-    console.log(response.avatar_url, );
+    // console.log(response.data.avatar_url, response.data.email);
+    console.log(response.data);
   })
   .catch(function (error) {
     // handle error
     console.log(error);
-  })
-// fetch('https://api.github.com/users/ariasadeghi530')
-//   .then(r => r.json())
-//   .then(data => {
-//     console.log(data)
-//   })
-//   .catch(e => console.error(e));
+  });
+
+
