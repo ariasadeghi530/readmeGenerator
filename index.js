0ea64c473fb0432cef571162b0ac814ba327bc83
Aria Sadeghi <ariasadeghi@gmail.com>
2020-02-11 15:23:32 -0800

    removed excess files

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const axios = require('axios');
+const inquirer = require('inquirer');
 
 
+// axios.get('https://api.github.com/users/ariasadeghi530')
+//   .then(function (response) {
+//     // handle success
+//     // console.log(response.data.avatar_url, response.data.email);
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   });
 
-axios.get('https://api.github.com/users/ariasadeghi530')
-  .then(function (response) {
-    // handle success
-    // console.log(response.data.avatar_url, response.data.email);
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  });
-
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'user',
+    message: 'Type your GitHub username'
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?'
+  }
+])
+.then(response => {
+  console.log(response);
+})
 
