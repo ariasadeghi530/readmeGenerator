6c3c349b1bdfe13825b7d247c24182d6144a376c
Aria Sadeghi <ariasadeghi@gmail.com>
2020-02-11 17:17:25 -0800

    did some stuff

--- index.js
+++ index.js
@@ -1,18 +1,57 @@
-const axios = require('axios');
+const userInfo = require('./utils/api.js');
 const inquirer = require('inquirer');
+const fs = require('fs');
+let user;
+let desciption;
+let title;
+let install;
+let info = [user, desciption, title, install];
 
 const questions = [
-
+"What is your GitHub username?",
+"What is the title of your project?",
+'Please enter a project description',
+"Please enter an installation guide"
 ];
 
 function writeToFile(fileName, data) {
+
 }
 
 function init() {
-
+  inquirer.prompt([
+    {
+      type: "input",
+      name: 'user',
+      message: questions[0]
+    },
+    {
+      type: "input",
+      name: 'title',
+      message: questions[1]
+    },
+    {
+      type: "input",
+      name: 'description',
+      message: questions[2]
+    },
+    {
+      type: "input",
+      name: 'install',
+      message: questions[3]
+    }
+  ])
+  .then( response => {
+    let user = response.user;
+    let title = response.title;
+    let desciption = response.description;
+    let install = response.install;
+  })
 }
 
 init();
+
+
 
 
 
--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,16 @@
+const axios = require('axios');
+
 const api = {
   getUser(username) {
-
+    axios.get(`https://api.github.com/users/${username}`)
+  .then(function ({data}) {
+    let userInfo = [data.avatar_url, data.email];
+    return userInfo;
+  })
+  .catch(function (error) {
+  
+    console.log(error);
+  });
   }
 };
 
