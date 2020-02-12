f9c3a70be44a0730709bd72ece19a1c3f8a3fe1b
Aria Sadeghi <ariasadeghi@gmail.com>
2020-02-12 15:09:31 -0800

    moved then call into index to avoid async issues

--- utils/api.js
+++ utils/api.js
@@ -1,16 +1,13 @@
 const axios = require('axios');
 
 const api = {
-  getUser(username) {
-    axios.get(`https://api.github.com/users/${username}`)
-  .then(function ({data}) {
-    let userInfo = [data.avatar_url, data.email];
-    return userInfo;
-  })
-  .catch(function (error) {
-  
-    console.log(error);
-  });
+   getUser(username) {
+    return axios.get(`https://api.github.com/users/${username}`)
+      
+      .catch(function (error) {
+
+        console.log(error);
+      });
   }
 };
 
